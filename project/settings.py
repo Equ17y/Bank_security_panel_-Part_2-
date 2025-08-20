c9f66609c2eff15f8c7572617a8d7b200164da73
Equ17y <antonsobol28@gmail.com>
2025-08-20 20:57:52 +0300

    Добавлено дефолтное значение

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = env.str('SECRET_KEY', 'REPLACE_ME')
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['localhost', '127.0.0.1'])
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES = [
